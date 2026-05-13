c35f8b788dbad4cfc933e70d5c50f403976452ca
Muhammad Faridz <faridzmuhamad679@gmail.com>
2026-05-13 20:05:03 +0700

    feat(design): migrate to Sen font + 21n tokens (warm-neutral palette, pill buttons, flat surfaces)

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
-import "@fontsource/plus-jakarta-sans/400.css";
-import "@fontsource/plus-jakarta-sans/500.css";
-import "@fontsource/plus-jakarta-sans/600.css";
-import "@fontsource/plus-jakarta-sans/700.css";
+import "@fontsource/sen/400.css";
+import "@fontsource/sen/500.css";
+import "@fontsource/sen/600.css";
+import "@fontsource/sen/700.css";
 
 import "antd/dist/antd.min.css";
 import "./global.css";
